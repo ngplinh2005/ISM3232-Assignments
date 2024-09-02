7bdcfdd62410cd94fd0f1a2a7793b6fcdc36d7dd
Nguyễn Phương Linh <ngplinh2005@gmail.com>
2024-09-01 21:03:00 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,5 @@
+// Task 1: Variables and Data Types
+
 let employeeName ="John Doe";
 const employeeID =12345
 var isActive = true; 
@@ -5,3 +7,12 @@ console.log(employeeName,typeof employeeName);
 console.log(employeeID, typeof employeeID);
 console.log(isActive, typeof isActive);
 
+// Task 2: Compound Data Types
+
+let products = ["shirt", "cup", "shoes"]
+const productDetails = {
+    productName: products,
+    price: 990.98,
+    inStock: true
+}
+console.log(products, productDetails)
